19aa0acd815969f1692d86c68f4f53eb67a11986
huulongn15 <huulongn15@gmail.com>
2023-12-20 11:27:44 +0700

    update: WORKFLOWS - Update subscriptionId

--- ACA.sh
+++ ACA.sh
@@ -18,24 +18,24 @@ az storage account create \
   --location "$LOCATION" \
   --sku Standard_RAGRS \
   --kind StorageV2
-
-QUEUE_CONNECTION_STRING=`az storage account show-connection-string -g $RESOURCE_GROUP --name $STORAGE_ACCOUNT_NAME --query connectionString --out json | tr -d '"'`
+#storageAccountConnectionString
+STORAGE_ACCOUNT_CONNECTION_STRING=`az storage account show-connection-string -g $RESOURCE_GROUP --name $STORAGE_ACCOUNT_NAME --query connectionString --out json | tr -d '"'`
 
 az storage queue create \
   --name 'myqueue' \
   --account-name $STORAGE_ACCOUNT_NAME \
-  --connection-string $QUEUE_CONNECTION_STRING
+  --connection-string $STORAGE_ACCOUNT_CONNECTION_STRING
 
 az storage message put \
   --content "Hello Queue Reader App" \
   --queue-name "myqueue" \
-  --connection-string $QUEUE_CONNECTION_STRING
+  --connection-string $STORAGE_ACCOUNT_CONNECTION_STRING
 
 az deployment group create --resource-group "$RESOURCE_GROUP" \
   --template-file ./queue.json \
   --parameters \
     environment_name="$CONTAINERAPPS_ENVIRONMENT" \
-    queueconnection="$QUEUE_CONNECTION_STRING" \
+    queueconnection="$STORAGE_ACCOUNT_CONNECTION_STRING" \
     location="$LOCATION"
 
 LOG_ANALYTICS_WORKSPACE_CLIENT_ID=`az containerapp env show --name $CONTAINERAPPS_ENVIRONMENT --resource-group $RESOURCE_GROUP --query properties.appLogsConfiguration.logAnalyticsConfiguration.customerId --out tsv`
